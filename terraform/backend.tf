958164c9b3c3ac51bd677390db44e1f3d971c29d
Waniya Faheem <waniyafaheem12@gmail.com>
2025-06-19 18:31:13 +0500

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "anas-terraform-state-bucket"
+    bucket       = "waniya-terraform-state-bucket"
     key          = "todo-swarm/terraform.tfstate"
     region       = "us-east-1"    
     encrypt      = true
